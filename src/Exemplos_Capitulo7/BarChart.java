e41da2d12ba39192986e6d71a9f07c62b87e4d50
IttsPedro <peterpereira204@gmail.com>
2026-01-22 21:31:23 +0000

    Continuação dos aprendizados e usos com array

---
+++ src/Exemplos_Capitulo7/BarChart.java
@@ -0,0 +1,27 @@
+package Exemplos_Capitulo7;
+// Figura 7.6: BarChart.java
+// programa de impressão de gráfico de barras
+
+public class BarChart {
+    public static void main(String[] args) {
+
+        int [] array = {0, 0 , 0, 0, 0, 0, 1, 2, 4, 2, 1 };
+
+        System.out.println("Grade distribuition: ");
+
+        // para cada elemento de array, gera saída de uma de uma barra do gráfico
+        for (int counter = 0; counter < array.length; counter++) {
+
+            // gera saída do rótulo de barra ("oo-09: ", ..., "90-99: ", "100: ")
+            if (counter == 10)
+                System.out.printf("%5d: ", 100);
+            else
+                System.out.printf("%02d-%02d: ", counter *10, counter *10 + 9);
+
+            // imprime a barra de asteriscos
+            for (int stars = 0; stars < array[counter]; stars++)
+
+                System.out.println();
+        }
+    }
+}
